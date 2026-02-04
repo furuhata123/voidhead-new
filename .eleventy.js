bf25f4c0747e49db30faba7635b0d14d5658f06c
furuhata123 <aerobikhttp@outlook.com>
2026-02-04 11:42:28 +0800

    major part?

---
+++ .eleventy.js
@@ -0,0 +1,48 @@
+const { DateTime } = require("luxon");
+const excerpt = require('eleventy-plugin-excerpt');
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({ "public/assets": "assets" });
+
+    eleventyConfig.addCollection("blog", function (collectionApi) {
+        return collectionApi.getFilteredByTag("blog");
+    });
+
+
+    // 摘要短代码
+    eleventyConfig.setFrontMatterParsingOptions({
+        excerpt: true,
+        excerpt_separator: "<!-- readmore -->",
+    });
+
+
+    eleventyConfig.addFilter("date", (value, format = "yyyy-MM-dd") => {
+        if (!(value instanceof Date)) {
+            return "Invalid Date";
+        }
+        return DateTime.fromJSDate(value, { zone: "utc" }).toFormat(format);
+    });
+
+    // tag页面生成
+    eleventyConfig.addCollection("tagList", function(collectionApi) {
+        const tagSet = new Set();
+        collectionApi.getAll().forEach(item => {
+          (item.data.tags || []).forEach(tag => {
+            if (tag !== "blog") tagSet.add(tag);
+          });
+        });
+        return [...tagSet];
+      });
+    
+
+
+
+    return {
+        dir: {
+            input: "src",
+            includes: "_includes",
+            output: "_site",
+        }
+
+    };
+
+}
